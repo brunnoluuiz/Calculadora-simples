54ac7ba45450283f848407fe405d059b8ffa5c4a
Bruno Oliveira <bluizsilva37@gmail.com>
2023-02-06 20:13:56 -0300

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+function calcular(n1,n2){
+    n1 = parseFloat(document.getElementById("n1").value)
+    n2 = parseFloat(document.getElementById("n2").value)
+    
+    selector = document.getElementById("selector").value
+
+    switch(selector){
+        case '+':
+        calculo = (n1 + n2)
+        document.getElementById("resultado").innerHTML = `O resultado da soma de ${n1} + ${n2} = ${calculo}`
+        break
+
+        case '-':
+        calculo = (n1 - n2)
+        document.getElementById("resultado").innerHTML = `O resultado da soma de ${n1} - ${n2} = ${calculo}`
+        break
+
+        case '*':
+        calculo = (n1 * n2)
+        document.getElementById("resultado").innerHTML = `O resultado da soma de ${n1} * ${n2} = ${calculo}`
+        break
+
+        case '/':
+        calculo = (n1 / n2)
+        document.getElementById("resultado").innerHTML = `O resultado da soma de  ${n1} / ${n2} = ${calculo}`
+        break
+    }
+}
